4722b18e2bb91d47d289619fc4f6425acd6010c5
lorchm <laure.chamouard@edu.ece.fr>
2019-05-01 19:48:26 +0200

    resol conflit

--- vendre.php
+++ vendre.php
@@ -48,18 +48,55 @@
     </nav>
 <!-- *********************************** PAGE VENDRE ***************************************************************** -->
 
-<!-- profil du vendeur -->
-<div class = "profil">
+<!-- ***** boutons + profil ***** -->
 
-<img src="Patrick.jpg">
 
-<p> Patrick </p>
+<div class="description page-header header container-fluid"> 
+    <h1> MES PRODUITS EN VENTE</h1> 
+    <div style="background-image: url('img/aya.jpg');">
+        <div class="row">
+            <div class="col-lg-4">
+                <br><br>
+                <button class="btt"> AJOUTER UN ARTICLE </button>
+                <button class="btt"> SUPPRIMER UN ARTICLE </button>
+            </div>
 
+        <div class="col-lg-7"></div>
+
+        <div class="col-lg-1"> 
+
+            <div class = "profil">
+
+                
+                <img src="img/shiba.jpg" class="roundedImage" style="width : 100px;height : 100px;">
+                <b class="pseud"> Patrick </b>
+
+            </div>
+        </div>
+
+        <!--*****  Liste des objets ***** -->
+        <div class="cadre-prod-2">
+            <div class="row">
+                <!-- 1. IMAGE -->
+                <div class="col-md-5">
+                    <a href="img/aya.jpg" target="_blank"><img src="img/aya.jpg" style="width: 100%; height: 185px;"></a>
+                </div>
+                <!-- 2. DETAILS ARTICLE -->
+                <div class="col-md-7" style="margin-top: 10px;">
+                    <div class="en-tete-prod">Album de Artiste</div>
+                    <div class="prix-prod">Mettre le prix &euro;</div>
+                    <div class="reference-prod">La référence RS55ZQ</div>
+                    <div class="description-prod">Description Aya la best.</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
-<!-- boutons add/suppr -->
 
-<!-- listing de tous les objets -->
+
+
+
 
 
 
