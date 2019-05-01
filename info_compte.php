23cbecb669d848db814f00a473b60d0d254d9e09
lorchm <laure.chamouard@edu.ece.fr>
2019-05-01 16:39:57 +0200

    quelques trucs

--- info_compte.php
+++ info_compte.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <link href="styles v2.css" rel="stylesheet" type="text/css" >
+    <link href="styles.css" rel="stylesheet" type="text/css" >
     <?php include('modif_cpte.php'); ?>
 
 
@@ -21,7 +21,7 @@
 
     <nav class="navbar navbar-expand-md">
 
-        <a class="navbar-brand" href="squelette.html"> <img src="logo.png"> </a>         
+        <a class="navbar-brand" href="squelette.html"> <img src="img/logo.png"> </a>         
         <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">      
             <span class="navbar-toggler-icon"></span>        
         </button>        
