605c8c6f3d32c3dddc6ebc7f41e1e71d097c3b63
lorchm <laure.chamouard@edu.ece.fr>
2019-05-02 16:57:18 +0200

    alors

--- vendre.php
+++ vendre.php
@@ -14,6 +14,7 @@
     <link href="styles.css" rel="stylesheet" type="text/css" />
 
 </head>
+
 <body>
     <!-- Barre de navigation -->
     <nav class="navbar navbar-expand-md">
@@ -54,49 +55,24 @@
 <div class="description page-header header container-fluid"> 
     <h1> MES PRODUITS EN VENTE</h1> 
     <div style="background-image: url('img/aya.jpg');">
-        <div class="row">
-            <div class="col-lg-4">
-                <br><br>
-                <button class="btt"> AJOUTER UN ARTICLE </button>
-                <button class="btt"> SUPPRIMER UN ARTICLE </button>
-            </div>
-
-            <div class="col-lg-7"></div>
-
-            <div class="col-lg-1"> 
+            <div class="col-lg-12"> 
                 <div class = "profil"> 
                     <img src="img/shiba.jpg" class="roundedImage" style="width : 100px;height : 100px;">
                     <b class="pseud"> Patrick </b>
                 </div>
             </div>
-        </div>
-
-        <!--*****  Liste des objets ***** -->
-        <div class="cadre-prod-2">
-            <div class="row">
-                <!-- 1. IMAGE -->
-                <div class="col-md-5">
-                    <a href="img/aya.jpg" target="_blank"><img src="img/aya.jpg" style="width: 100%; height: 185px;"></a>
-                </div>
-                <!-- 2. DETAILS ARTICLE -->
-                <div class="col-md-7" style="margin-top: 10px;">
-                    <div class="en-tete-prod">Album de Artiste</div>
-                    <div class="prix-prod">Mettre le prix &euro;</div>
-                    <div class="reference-prod">La référence RS55ZQ</div>
-                    <div class="description-prod">Description Aya la best.</div>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 
 
 
 
+
+
 <!--******************** PIED DE PAGE *********************************************************************************-->
     <footer class="page-footer">
-       <div class="container">
-           <div class="row">
+        <div class="container">
+            <div class="row">
                 <div class="col-lg-4">
                 <p style="color:#ffa630; margin-left: 50px;">INFORMATIONS ADDITIONNNELLES</p><p style="margin-left: 50px;">Notre equipe<br>Laure CHAMOUARD<br>Barbara GERMANEAU<br>Victor LABEYRIE</p>
                 </div>
@@ -104,13 +80,13 @@
                     <img src="img/shiba_footer.png" style="margin-left:100px;margin-top: 20px; width:90px;height: 90px;">
                 </div>
                 <div class="col-lg-4">
-                   <p style="color:#ffa630;">CONTACT</p><p>
-                       37, quai de Grenelle, 75015 Paris, France <br>
-                       <a href="mailto:ece.amazon@pro.com" style="color:#ffa630;"> ece.amazon@pro.com</a> <br>
-                       +33 01 02 03 04 05 <br>
-                       +33 01 03 02 05 04
-                   </p>
-               </div>
+                    <p style="color:#ffa630;">CONTACT</p><p>
+                        37, quai de Grenelle, 75015 Paris, France <br>
+                        <a href="mailto:ece.amazon@pro.com" style="color:#ffa630;"> ece.amazon@pro.com</a> <br>
+                        +33 01 02 03 04 05 <br>
+                        +33 01 03 02 05 04
+                    </p>
+                </div>
             </div>
             <div class="footer-copyright text-center" style="text-align: center;">&copy; 2019 ECE Amazon | Droit d'auteur: BLV</div>
         </div>
