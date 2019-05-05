979df7a3660c6932126bdf160987b793e1dd0491
Viviiii <>
2019-05-05 19:27:23 +0200

    Fichiers pour le mail

--- payer.php
+++ payer.php
@@ -78,8 +78,8 @@
 
                 <?php
 
-                ini_set("SMTP", "smtp.orange.fr");
-                ini_set("smpt_port", 25);// sachant que le port ressemblera sûrement à quelquechose comme 8025
+             /*   ini_set("SMTP", "smtp.orange.fr");
+                ini_set("smpt_port", 25);// sachant que le port ressemblera sûrement à quelquechose comme 8025*/
 
                 $sql1="SELECT email FROM acheteur WHERE pseudo LIKE '".$N."'";
                 $result1 = mysqli_query($db_handle, $sql1);
@@ -87,42 +87,52 @@
                     {
                         $mail=$data1['email'];//'adresse de destination
                     }
+                    echo"$mail";
 
             
-                    if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) // On filtre les serveurs (nécessaire au bon fonctionnement de l'envoi)
+                  /*  if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) // On filtre les serveurs (nécessaire au bon fonctionnement de l'envoi)
                     {
                         $passage_ligne = "\r\n";
                     }
                     else
                     {
                         $passage_ligne = "\n";
-                    }
+                    }*/
                     //=====Déclaration des messages au format texte et au format HTML.
-                    $message_txt = "Bonjour, nous vous informons que votre commande a bien été prise en compte, merci et à bientôt sur ECE Amazon";
-                     
                     //=====Création de la boundary
-                    $boundary = "-----=".md5(rand());
+                   /* $boundary = "-----=".md5(rand());*/
                     //==========
                      
+
                     //=====Définition du sujet.
                     $sujet = "Confirmation de votre commande ECE Amazon";
                     //=========
                      
                     //=====Création du header de l'e-mail.
-                    $header = "From: \"ECEAmazon\"<victor.labeyrie@edu.ece.fr>".$passage_ligne;
-                    $header.= "Reply-to: \"ECEAmazon\" <victor.labeyrie@edu.ece.fr>".$passage_ligne;
-                    $header.= "MIME-Version: 1.0".$passage_ligne;
-                    $header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+                    $header = "From: \"ECEAmazon\"<eceamazonblv@gmail.com>"."\n";
+                    $header.= 'Content-Type: text/html; charset="utf-8"'."\n";
+                    $header.= "MIME-Version: 1.0\r\n";
+                    $header.= "Content-Transfer-Encoding: 8bit";
+                   
                     //==========
-                     
-                    //=====Création du message.
+                     $message='
+                     <html>
+
+                        <body>
+                            <div align=center>
+                                Bonjour, nous vous informons que votre commande a bien été prise en compte, merci et à bientôt sur ECE Amazon
+                            </div>
+                        </body>
+                    </html>
+                    ';
+                   /* //=====Création du message.
                     $message = $passage_ligne."--".$boundary.$passage_ligne;
                     //=====Ajout du message au format texte.
-                    $message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
-                    $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+                   
+                    
                     $message.= $passage_ligne.$message_txt.$passage_ligne;
                     //==========
-                    $message.= $passage_ligne."--".$boundary.$passage_ligne;
+                    $message.= $passage_ligne."--".$boundary.$passage_ligne;*/
                     //=====Ajout du message au format HTML
                     //=====Envoi de l'e-mail.
                     mail($mail,$sujet,$message,$header);
